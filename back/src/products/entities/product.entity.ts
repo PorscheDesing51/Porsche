fc3f6dca12a15f12b9ed72a5550b0462f84dd5e7
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2025-04-09 22:00:22 -0500

    Se configura DTO Products

--- back/src/products/entities/product.entity.ts
+++ back/src/products/entities/product.entity.ts
@@ -1,1 +1,27 @@
-export class Product {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({
+    name: "products"
+})
+export class Product {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    model: string;
+
+    @Column()
+    price: number;
+
+    @Column()
+    description: string;
+
+    @Column()
+    createdAt: Date;
+
+    @Column()
+    updatedAt: Date;
+}
